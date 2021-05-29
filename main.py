7ea07fc4032506c013323cef5c94ae41c9442fac
Priyanshu Kumar <i.priyanshukumar01@gmail.com>
2021-05-29 16:44:26 +0530

    Modules in Python

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import calculator
+from maths import *
 
-print(calculator.add(3, 2))
-print(calculator.div(3, 2))
-print(calculator.mul(3, 2))
-print(calculator.sub(3, 2))
+print(simple.add(1, 2))
---
+++ maths/complex.py
@@ -0,0 +1,6 @@
+def sqaure(a):
+    return a**2
+
+
+def cube(a):
+    return a**3
